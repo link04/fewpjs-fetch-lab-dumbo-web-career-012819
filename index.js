4faee25946ff79e4f06c5118c83cf22652efce58
Maximo Bautista <max.bautista97@gmail.com>
2019-03-18 16:21:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function fetchBooks() {
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
+  
 }
 
 function renderBooks(json) {
@@ -13,3 +17,8 @@ function renderBooks(json) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
 })
+
+
+  
+  
+  
